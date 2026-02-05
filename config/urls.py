d7c612722126f68ced54d67d4cac29df912de1d2
satyasai120 <andesatyasai@gmail.com>
2026-02-05 16:02:04 +0530

    Add home route for Render

--- config/urls.py
+++ config/urls.py
@@ -21,3 +21,15 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("feed.urls")), 
 ]
+
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Playto Engineering Challenge API is running successfully 🚀")
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),
+]
